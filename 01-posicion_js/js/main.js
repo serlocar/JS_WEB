a990d876b0b941dca7d96d44c308151d74625046
serlocar <sergio.lopez.cardin@tajamar365.com>
2017-05-23 21:28:14 +0200

    actualizado

---
+++ 01-posicion_js/js/main.js
@@ -0,0 +1,20 @@
+//const oFormulario = new Formulario("Luis", "Desarrollo Web full-stack");
+//oFormulario.controller()
+
+
+
+//window.onload = function () {
+//    new Formulario("Luis", "Desarrollo Web full-stack").controller()
+//};
+
+
+//document.addEventListener("DOMContentLoaded", function () {
+//    new Formulario("Luis", "Desarrollo Web full-stack").controller()
+//})
+
+
+document.addEventListener("DOMContentLoaded", main);
+
+function main() {
+    new Formulario("Luis", "Desarrollo Web full-stack").controller()
+}
---
+++ 02-jq/js/main.js
@@ -0,0 +1,8 @@
+// En JS serÃ­a window.onload = 
+// En JQ serÃ­a $(document).ready() 
+// En JQ hay un atajo del anterior
+$(function() {
+    let sName = "Luis";
+    let sCurso = "Desarrollo Web full-stack";
+    new Formulario("Luis", "Desarrollo Web full-stack").controller()
+})
