79c3443d6edd7d790b98e0b865641924bbc0bb64
serlocar <sergio.lopez.cardin@tajamar365.com>
2017-05-26 21:25:01 +0200

    actualizado

--- 14-formulario/js/main.js
+++ 14-formulario/js/main.js
@@ -1,3 +1,14 @@
-$(function() {
+class Formulario {
+    constructor() {
+        this.aCurso = document.querySelectorAll("[name = curso]")
+        this.aSalida = []
+    }
+    getRRadiobuttons() {
+        for (let i = 0; i < this.aCurso.length; i++) {
+            if (this.aCurso[i].checked) {
+                this.aSalida[this.aSalida.length]
+            };
+        };
+    };
 
-})
+}
